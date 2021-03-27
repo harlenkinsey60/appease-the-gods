3901522e8a532cdd2bc84af6eb8c0f1d6577538e
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-03-26 21:39:00 -0500

    Added Inventory Item Class
    -Added inventory item class for use with player inventory.

---
+++ Appease the Gods/Assets/resources/InventoryItem/InventoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InventoryItem
+{
+    public string Name;
+    public int Count;
+    public int WoodDamage;
+    public int StoneDamage;
+    public int MetalDamage;
+    public int Durability;
+    public string Type;
+
+    public InventoryItem(string name, int count, int woodDamage, int stoneDamage, int metalDamage, int durability, string type)
+    {
+        Name = name;
+        Count = count;
+        WoodDamage = woodDamage;
+        StoneDamage = stoneDamage;
+        MetalDamage = metalDamage;
+        Durability = durability;
+        Type = type;
+    }
+
+}

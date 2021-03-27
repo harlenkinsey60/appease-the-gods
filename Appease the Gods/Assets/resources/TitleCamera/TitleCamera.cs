f262ba4238222a52e8995247b841764a3b408d12
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-03-26 21:43:21 -0500

    Added Title Scene
    -Created UI and UI controllers for title scene as well as a cinematic camera and fake world generation.

---
+++ Appease the Gods/Assets/resources/TitleCamera/TitleCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCamera : MonoBehaviour
+{
+    GameObject Shrek;
+
+    void Start()
+    {
+        Shrek = GameObject.Find("Shrek");
+    }
+
+    void Update()
+    {
+        transform.RotateAround(Shrek.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
---
+++ Appease the Gods/Assets/resources/TitleUI/TitleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    SoundManager SoundManager;
+
+    void Start()
+    {
+        SoundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        SoundManager.VolumeSlider = transform.Find("Volume").GetComponent<Slider>();
+        SoundManager.Initialize();
+        SoundManager.Play("Title");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}

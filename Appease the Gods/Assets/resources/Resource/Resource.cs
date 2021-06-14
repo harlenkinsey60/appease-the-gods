80ff9e6ce114d9a0491a02c2c1d09aecbc97aafc
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-14 17:18:20 -0500

    Added Resource Component
    - For use with Tree, Stone, and Metal
    - Interface used to trigger decay and get resource type

---
+++ Appease the Gods/Assets/resources/Resource/Resource.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public string Type;
+    private float Health;
+    private Animator ResourceAnimator;
+
+    // Getters and Setters
+
+    public string GetType()
+    {
+        return Type;
+    }
+
+    public void SetHealth(float health)
+    {
+        Health = health;
+    }
+
+    public float GetHealth()
+    {
+        return Health;
+    }
+
+    private void Decay()
+    {
+        ResourceAnimator.SetBool("Decay", true);
+    }
+
+//Add unity event to call destroy
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Monobehaviour Methods
+
+    void Start()
+    {
+        Debug.Log("Resource needs to implement animation event for Destroy()");
+    }
+
+}

63b97770dc206100cc366a22cc5aa818ec676efe
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-14 17:15:46 -0500

    PlayerHealth Component
    - Interface for the player's health and triggering death

---
+++ Appease the Gods/Assets/resources/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int Health {get; set;}
+
+    void Start(){
+        Health = 100;
+    }
+
+    public bool CheckIfDead(){
+        if(Health < 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}

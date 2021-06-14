6b6302b45ffe883d713a3747c014cf1b3a49df73
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-14 17:19:31 -0500

    Added PlayerState Component
    - Handles everything regarding the player's state (input, animation, etc.)

---
+++ Appease the Gods/Assets/resources/Player/PlayerState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public string State {get; set;}
+
+    void Start()
+    {
+        State = "Idle";
+    }
+
+    void Update()
+    {
+        HandleState();
+    }
+
+    private void HandleState(){
+        switch(State){
+            case "Idle":
+                break;
+            case "Walking":
+                break;
+            case "Running":
+                break;
+            case "Jumping":
+                break;
+            case "Harvesting":
+                break;
+            case "Taking_Damage":
+                break;
+            case "Dead":
+                break;
+        }
+    }
+}

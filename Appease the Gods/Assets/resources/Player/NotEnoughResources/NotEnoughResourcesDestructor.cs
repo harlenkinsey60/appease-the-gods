013be58a58aa31222aa5dad9125eddf7e55517ab
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-14 17:14:23 -0500

    "Not enough resources." UI message

---
+++ Appease the Gods/Assets/resources/Player/NotEnoughResources/NotEnoughResourcesDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotEnoughResourcesDestructor : MonoBehaviour
+{
+    private float TimeToDestruction = 3.0f;
+
+    void Update()
+    {
+        if(TimeToDestruction > 0){
+            TimeToDestruction -= Time.deltaTime;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+}

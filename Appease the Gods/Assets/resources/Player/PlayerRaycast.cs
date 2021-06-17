63a07fe7d6f3e920b72cbec7fe82a409d4b4e1a4
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-16 19:51:44 -0500

    Added PlayerRaycast Component
    - Returns a gameobject captured from a raycast that originates from the center of the
      player's screen

---
+++ Appease the Gods/Assets/resources/Player/PlayerRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+
+    private GameObject RecentlyHitGameObject;
+
+    // if Raycast hits something then sets RecentlyHitGameObject to recently
+    // hit gameobject and to null if it hits nothing
+
+    public void SetObject(){
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0)); // shoots ray from center of screen
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            RecentlyHitGameObject = hit.collider.gameObject;
+        }
+        else {
+            RecentlyHitGameObject = null;
+        }
+
+    }
+
+    // returns RecentlyHitObject for use with other components
+
+    public GameObject GetObject()
+    {
+        return RecentlyHitGameObject;
+    }
+
+}

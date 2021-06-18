142ccdaa6acf41cbfd731ef7418ba2882fda03c7
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-06-17 21:50:05 -0500

    Added shrek decay animation and approprate components
    - Added ShrekHealth and ShrekState components
    - Removed ShrekController and ShrekData components

---
+++ Appease the Gods/Assets/resources/Shrek/ShrekHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrekHealth : MonoBehaviour
+{
+    private float Health;
+
+    // Getters and Setters
+
+    public float GetHealth()
+    {
+        return Health;
+    }
+
+    public void SetHealth(float health)
+    {
+        Health = health;
+    }
+
+    // Monobehaviour Functions
+
+    void Start()
+    {
+        SetHealth(1000.0f);
+    }
+}
---
+++ Appease the Gods/Assets/resources/Shrek/ShrekState.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrekState : MonoBehaviour
+{
+    Animator ShrekAnimator;
+    ShrekHealth ShrekHealth;
+    PlayerSoundManager PlayerSoundManager;
+    private string State;
+
+    // Getters and Setters
+
+    public string GetState()
+    {
+        return State;
+    }
+
+    public void SetState(string state)
+    {
+        State = state;
+    }
+
+    // Handles Animations and Updates Necessary Components
+
+    private void HandleState()
+    {
+        switch(State)
+        {
+            case "Idle":
+
+                ShrekAnimator.SetBool("IsDancing", true);
+                ShrekAnimator.SetBool("IsTPosing", false);
+
+                break;
+
+            case "Decay":
+
+                ShrekAnimator.SetBool("Decay", true);
+
+                break;
+        }
+
+        // Handles Death
+
+        if(ShrekHealth.GetHealth() <= 0.0f)
+        {
+            SetState("Decay");
+        }
+    }
+
+    public void PlayDecaySound()
+    {
+        PlayerSoundManager.PlaySound("ShrekSoundOne");
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Monobehaviour Functions
+
+    void Start()
+    {
+        ShrekAnimator = GetComponent<Animator>();
+        ShrekHealth = GetComponent<ShrekHealth>();
+        PlayerSoundManager = GameObject.Find("Player").GetComponent<PlayerSoundManager>();
+
+        SetState("Idle");
+    }
+
+    void Update()
+    {
+        HandleState();
+    }
+}
